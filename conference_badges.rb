313fb0c39a93df34248e56933d724972ff6bd47e
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-09 10:09:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,23 +8,23 @@ def badge_maker(array,names)
   return badge_maker
 end
 
-def batch_badge_creator(array,speaker)
-  conf_speaker = []
-  array.each do |x|
-  conf_speaker << (x)
-return batch_badge_creator
-end
+# def batch_badge_creator(array,speaker)
+#   conf_speaker = []
+#   array.each do |x|
+#   conf_speaker << (x)
+# return batch_badge_creator
+# end
 
 
-def assign_rooms(array, rooms)
-  conf_speaker[] = rooms
-  rooms.index do |rooms|
-  conf_speaker << rooms
-return badge_maker
-end
+# def assign_rooms(array, rooms)
+#   conf_speaker[] = rooms
+#   rooms.index do |rooms|
+#   conf_speaker << rooms
+# return badge_maker
+# end
 
 
-def printer
-  prints batch_badge_creator
-  prints assign_rooms
-end
+# def printer
+#   prints batch_badge_creator
+#   prints assign_rooms
+# end
