25052b2acb962ec067c1a37526757c3e438428cb
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-09 08:38:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,11 @@
 #output the results in diff. ways
 
 
-def badge_maker[name]
+def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-return badge_maker{:names}
+badge_maker
 
 def batch_badge_creator(array)
   conf_speaker = []
