fcdfedd2f567b14c309d1895f3da239fcda0e497
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-09 10:43:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def batch_badge_creator(attendees)
       badge_names = []
       attendees.each do |attendee|
-      badges << "Hello, my name is #{name}."  
+      badges_names << "Hello, my name is #{name}."  
   end
 end  
   
