652589ae734aa5e3a18c96a09a2bd7017fc70dc9
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-09 12:05:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,10 +20,9 @@ def assign_rooms(attendees)
   rooms = []
   index = 1
   attendees.each_with_index do |attendee, index|
-  rooms << "Hello, #{attendees}, you will be assigned to room #{rooms[index]}"
-  index += 1
+  rooms << "Hello, #{attendees}, you will be assigned to room #{rooms[index += 1]}"
   end
-badge_maker
+rooms
 end
 
 
