c46b80cc702843ac3b75ebc5a2208182367505e6
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-09 08:43:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,13 +2,13 @@
 #output the results in diff. ways
 
 
-def badge_maker(array)
+def badge_maker(array,names)
   array = names
   puts "Hello, my name is #{name}."
 return badge_maker
 
 
-def batch_badge_creator(array)
+def batch_badge_creator(array,speaker)
   conf_speaker = []
   array.each do |x|
   conf_speaker << (x)
@@ -18,7 +18,7 @@ batch_badge_creator
 
 
 
-def assign_rooms(rooms)
+def assign_rooms(array, rooms)
   conf_speaker[] = rooms
   rooms.index do |rooms|
   conf_speaker << rooms
